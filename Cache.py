e2e2cfce40784669f127fc48bb6ed66e276b76c9
Ring <euria@live.cn>
2020-06-17 18:03:49 +0800

    Update Cache.py
    Define insert and update funtions

--- Cache.py
+++ Cache.py
@@ -15,6 +15,29 @@ class Cache:
         self.cache = defaultdict(set)
         self.size = 10e6
         
-    
+    def insert(self, k, v):
+        """
+        k present the key,
+        v is the value to be inserted.
+        Insert value into cache dictionary
+        """
         
+        self.cache[k] = v
+        
+        return
+    
+    
+    def update(self, k, v):
+        """
+        k present the key,
+        v is the value to be updated.
+        Update the self.cache
+        """
+        if not self.cache[k]:
+            return "Not exists"
+        else:
+            self.cache[k] = v
+            
+            
+
         
