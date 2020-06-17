ef917799051a599dcfabda8a021bd4869f195545
Ring <euria@live.cn>
2020-06-17 17:07:54 +0800

    Create Cache.py
    Create Cache Class

---
+++ Cache.py
@@ -0,0 +1,15 @@
+# Cache Class
+
+
+class Cache:
+    """
+    In this class, I will provide the basic caching funtions.
+    """
+    
+    def __init__(self):
+        """
+        Construct Cache Ojbect with the following variables.
+        - cache
+        - size 
+        """
+        
